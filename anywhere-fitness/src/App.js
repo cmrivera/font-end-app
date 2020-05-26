51f85df38fbe4f29a0e410bd4622fae899e2a966
cmrivera <cmrivera0004@student.stcc.edu>
2020-05-26 12:46:59 -0400

    working

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
+import axios from "axios";
 import PrivateRoute from "./Utils/PrivateRoute";
 import PublicRoute from "./Utils/PublicRoute";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Home from "./components/Home";
-import "./index.css";
+
+import { getToken, removeUserSession, setUserSession } from "./Utils/Common";
 
 function App() {
+  const [authLoading, setAuthLoading] = useState(true);
+
+  useEffect(() => {
+    const token = getToken();
+    if (!token) {
+      return;
+    }
+
+    axios
+      .get(`https://anywhere-fitness-ptbw.herokuapp.com/api/auth/login/user`)
+      .then((response) => {
+        setUserSession(response.data.token, response.data.user);
+        setAuthLoading(false);
+      })
+      .catch((error) => {
+        removeUserSession();
+        setAuthLoading(false);
+      });
+  }, []);
+
+  if (authLoading && getToken()) {
+    return <div className="content">Checking Authentication...</div>;
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
