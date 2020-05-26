100e41259da2d811061da6ea31ab6745d16c7b36
cmrivera <cmrivera0004@student.stcc.edu>
2020-05-26 11:15:10 -0400

    changed a bit

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,21 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-import MyClassBoard from "./components/classBoard";
-import PrivateRoute from "./components/privateRoute";
-import Login from "./components/loginPage";
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
+import PrivateRoute from "./Utils/PrivateRoute";
+import PublicRoute from "./Utils/PublicRoute";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+import "./index.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/loginPage" component={Login} />
-          <Route component={Login} />
-          <PrivateRoute exact path="/classBoard" component={MyClassBoard} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink exact activeClassName="active" to="/">
+              Home
+            </NavLink>
+            <NavLink activeClassName="active" to="/login">
+              Login
+            </NavLink>
+            <small>(Access without token only)</small>
+            <NavLink activeClassName="active" to="/dashboard">
+              Dashboard
+            </NavLink>
+            <small>(Access with token only)</small>
+          </div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <PublicRoute path="/login" component={Login} />
+              <PrivateRoute path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
